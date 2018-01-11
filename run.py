dfe4e25b8d9318092931f39df6bc82c6a8723b6f
Wouter <wouter-vrielink@hotmail.com>
2018-01-11 17:46:56 +0100

    cleanup (not done) van animatie

--- run.py
+++ run.py
@@ -5,17 +5,11 @@ from matplotlib import animation
 import matplotlib.patches as patches
 import itertools
 
-width = 20
-height = 20
-steps = 2000
-ant_size = 0.4
-
-
 def grid_to_array(pos, width, height):
     return pos[0] - 0.5, height - pos[1] - 1.5
 
 
-def store_state(i, colony_positions, food_positions, ant_positions, ant_foods):
+def store_state(i, pheromones, colony_positions, food_positions, ant_positions, ant_foods):
     # append the pheromone distribution
     pheromones.append(np.rot90(np.copy(env.pheromones)))
 
@@ -36,7 +30,7 @@ def store_state(i, colony_positions, food_positions, ant_positions, ant_foods):
         food_positions[i].append(grid_to_array((x, y), width, height))
 
 
-def init_figure():
+def init_figure(pheromones, colony_positions, food_positions, ant_positions, ant_foods):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     im = ax.imshow(np.zeros((width, height)), vmin=0, vmax=np.max([np.max(p) for p in pheromones]),
@@ -62,48 +56,108 @@ def init_figure():
     return fig, ax, im, colony_patches, food_patches, ant_patches
 
 
-def animate(i):
-    im.set_array(pheromones[i].astype(np.float64).reshape(width, height))  # update the data
-    for j, colony_patch in enumerate(colony_patches):
-        colony_patch.set_xy(colony_positions[i][j])
-    for j, food_patch in enumerate(food_patches):
-        food_patch.set_xy(food_positions[i][j])
-    for j, ant_patch in enumerate(ant_patches):
-        ant_pos = ant_positions[i][j]
-        ant_pos = (ant_pos[0] + (1 - ant_size) / 2, ant_pos[1] + (1 - ant_size) / 2)
-        ant_patch.set_xy(ant_pos)
-        if ant_foods[i][j]:
-            ant_patch.set_facecolor('g')
-            ant_patch.set_edgecolor('g')
-            ant_patch.set_fill(True)
-        else:
-            ant_patch.set_facecolor('k')
-            ant_patch.set_edgecolor('k')
-            ant_patch.set_fill(False)
-    plt.title('iteration: ' + str(i))
-    fig.canvas.draw()
-    return im
+def compute_then_plot(env, steps):
+    pheromones = []
+    ant_positions = [[] for _ in range(steps + 1)]
+    colony_positions = [[] for _ in range(steps + 1)]
+    food_positions = [[] for _ in range(steps + 1)]
+    ant_foods = [[] for _ in range(steps + 1)]
+
+    store_state(0, pheromones, colony_positions, food_positions, ant_positions, ant_foods)
+    for i in range(1, steps + 1):
+        # take a step
+        env.step()
+
+        # store the state for animation
+        store_state(i, pheromones, colony_positions, food_positions, ant_positions, ant_foods)
+
+    fig, ax, im, colony_patches, food_patches, ant_patches = init_figure(pheromones, colony_positions, food_positions, ant_positions, ant_foods)
+
+    def animate(i):
+        im.set_array(pheromones[i].astype(np.float64).reshape(width, height))  # update the data
+        for j, colony_patch in enumerate(colony_patches):
+            colony_patch.set_xy(colony_positions[i][j])
+        for j, food_patch in enumerate(food_patches):
+            food_patch.set_xy(food_positions[i][j])
+        for j, ant_patch in enumerate(ant_patches):
+            ant_pos = ant_positions[i][j]
+            ant_pos = (ant_pos[0] + (1 - ant_size) / 2, ant_pos[1] + (1 - ant_size) / 2)
+            ant_patch.set_xy(ant_pos)
+            if ant_foods[i][j]:
+                ant_patch.set_facecolor('g')
+                ant_patch.set_edgecolor('g')
+                ant_patch.set_fill(True)
+            else:
+                ant_patch.set_facecolor('k')
+                ant_patch.set_edgecolor('k')
+                ant_patch.set_fill(False)
+        plt.title('iteration: ' + str(i))
+        fig.canvas.draw()
+        return im
+
+    ani = animation.FuncAnimation(fig, animate, steps, interval=1)
+    plt.figure()
+    plt.plot(env.min_path_lengths)
+    plt.ylim(ymin=0)
+    plt.show()
+
+def plot_continuous(env, steps = 1000):
+    pheromones = []
+
+    ant_positions = [[] for _ in range(steps + 1)]
+    colony_positions = [[] for _ in range(steps + 1)]
+    food_positions = [[] for _ in range(steps + 1)]
+    ant_foods = [[] for _ in range(steps + 1)]
+
+    store_state(0, pheromones, colony_positions, food_positions, ant_positions, ant_foods)
+
+    fig, ax, im, colony_patches, food_patches, ant_patches = init_figure(pheromones, colony_positions, food_positions, ant_positions, ant_foods)
+
+    im = ax.imshow(np.zeros((width, height)), vmin=0, vmax=100,
+                       interpolation='None',
+                       cmap="Purples")
 
 
-env = Environment(width=width, height=height, n_colonies=1, n_ants=100, decay=0.99, sigma=0.2, moore=True)
+    plt.ion()
+    fig.show()
 
-pheromones = []
-ant_positions = [[] for _ in range(steps + 1)]
-colony_positions = [[] for _ in range(steps + 1)]
-food_positions = [[] for _ in range(steps + 1)]
-ant_foods = [[] for _ in range(steps + 1)]
-store_state(0, colony_positions, food_positions, ant_positions, ant_foods)
-for i in range(1, steps + 1):
-    # take a step
-    env.step()
+    for i in range(steps):
+        plt.title('iteration: ' + str(i))
+        plt.pause(0.001)
 
-    # store the state for animation
-    store_state(i, colony_positions, food_positions, ant_positions, ant_foods)
+        # take a step
+        env.step()
 
-fig, ax, im, colony_patches, food_patches, ant_patches = init_figure()
-ani = animation.FuncAnimation(fig, animate, steps, interval=1)
-plt.figure()
-plt.plot(env.min_path_lengths)
-plt.ylim(ymin=0)
-plt.show()
+        # store the state for animation
+        store_state(i + 1, pheromones, colony_positions, food_positions, ant_positions, ant_foods)
 
+        im.set_array(pheromones[i].astype(np.float64).reshape(width, height))  # update the data
+        for j, colony_patch in enumerate(colony_patches):
+            colony_patch.set_xy(colony_positions[i][j])
+        for j, food_patch in enumerate(food_patches):
+            food_patch.set_xy(food_positions[i][j])
+        for j, ant_patch in enumerate(ant_patches):
+            ant_pos = ant_positions[i][j]
+            ant_pos = (ant_pos[0] + (1 - ant_size) / 2, ant_pos[1] + (1 - ant_size) / 2)
+            ant_patch.set_xy(ant_pos)
+            if ant_foods[i][j]:
+                ant_patch.set_facecolor('g')
+                ant_patch.set_edgecolor('g')
+                ant_patch.set_fill(True)
+            else:
+                ant_patch.set_facecolor('k')
+                ant_patch.set_edgecolor('k')
+                ant_patch.set_fill(False)
+
+        fig.canvas.draw()
+
+if __name__ == '__main__':
+    width = 20
+    height = 20
+    steps = 200
+    ant_size = 0.4
+
+    env = Environment(width=width, height=height, n_colonies=1, n_ants=100, decay=0.99, sigma=0.2, moore=True)
+
+    compute_then_plot(env, steps)
+    # plot_continuous(env, steps=steps)
