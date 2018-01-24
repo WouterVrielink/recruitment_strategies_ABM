c43bbf5a74c692718d25b4b94b7c106644f059f2
Maarten van der Sande <maartenvandersande@hotmail.com>
2018-01-24 16:08:38 +0100

    Roles are instances, not ints

--- rec_strat/model.py
+++ rec_strat/model.py
@@ -6,13 +6,14 @@ from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
 
 from ant import Ant
+from roles import Unassigned, Follower, Leader, Pheromone
 
 
 class Environment(Model):
     """ A model which contains a number of ant colonies. """
 
     def __init__(self, N=100, g=10, w=10, h=10, p_uf=0.5, p_pu=0.01, p_up=0.5, p_fl=0.01, p_lu=0.01,
-                 role_division=(100, 0, 5, 5),
+                 role_division={Unassigned: 100, Follower: 0, Leader: 5, Pheromone: 5},
                  moore=False):
         """
 
@@ -29,7 +30,14 @@ class Environment(Model):
         self.height = h
         self.moore = moore
         self.grid = MultiGrid(w, h, torus=True)
-        self.transition_p = [(0, 0), (0, 1), (p_uf, 1), (p_up, 3), (p_fl, 2), (p_pu, 0), (p_lu, 0)]
+        # self.transition_p = [(0, 0), (0, 1), (p_uf, 1), (p_up, 3), (p_fl, 2), (p_pu, 0), (p_lu, 0)]
+        self.recruit_probs = {Unassigned: (-1, None),
+                              Follower: (-1, None),
+                              Leader: (p_uf, Follower),
+                              Pheromone: (p_up, Pheromone)}
+        self.event_probs = {"succes": (p_fl, Leader),
+                            "failure": (p_lu, Unassigned),
+                            "scent_lost": (p_pu, Unassigned)}
 
         # Environment attributes
         self.schedule = RandomActivation(self)
@@ -38,8 +46,8 @@ class Environment(Model):
         self.g = g
         self.role_division = role_division
         self.N = np.sum(role_division)  # give amount of ants a variable N
-        for i, number in enumerate(role_division):
-            self.add_ants(number, i)
+        for role, number in role_division.items():
+            self.add_ants(number, role)
 
         model_reporters = {"unassigned": lambda m: sum([1 if a.role == 0 else 0 for a in m.schedule.agents]),
                            "followers": lambda m: sum([1 if a.role == 1 else 0 for a in m.schedule.agents]),
