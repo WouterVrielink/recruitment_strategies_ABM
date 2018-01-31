68717f0944f3b1758e56a82ccfee42d67750e8f1
Wouter <wouter-vrielink@hotmail.com>
2018-01-31 17:23:32 +0100

    Docstrings added for model.py

--- rec_strat/model.py
+++ rec_strat/model.py
@@ -10,18 +10,25 @@ from roles import Unassigned, Follower, Leader, Pheromone
 
 
 class Environment(Model):
-    """ A model which contains a number of ant colonies. """
+    """ A model which contains ants with specified roles. """
 
     def __init__(self, N=100, g=2, w=10, h=10, p_uf=0.5, p_pu=0.1, p_up=0.5, p_fl=0.8, p_lu=0.05,
                  role_division={Unassigned: 10, Follower: 0, Leader: 5, Pheromone: 5},
                  moore=False, grow=False):
         """
-
-        :param g: amount of ants possible in a following group of ants
-        :param width: int, width of the system
-        :param height: int, height of the system
-        :param role_division: tuple with (nr_unassigned, nr_followers, nr_leaders, nr_pheromoners)
-        :param moore: boolean, True/False whether Moore/vonNeumann is used
+        Args:
+            N: number of ants
+            g: amount of ants possible in a following group of ants
+            w: width of the system
+            h: height of the system
+            p_uf: the probability that Unassigned changes to Follower
+            p_pu: the probability that Pheromone changes to Unassigned
+            p_up: the probability that Unassigned changes to Pheromone
+            p_fl: the probability that Follower changes to Leader
+            p_lu: the probability that Leader changes to Unassigned
+            role_division: dictionary that holds number of ants assigned to specific roles
+            moore: True/False whether Moore/vonNeumann is used
+            grow: True/False whether the system grows over time or not
         """
         super().__init__()
 
@@ -46,7 +53,8 @@ class Environment(Model):
         # Ant variables
         self.g = g
         self.role_division = role_division
-        self.N = np.sum(role_division)  # give amount of ants a variable N
+        self.N = np.sum(role_division)
+
         for role, number in role_division.items():
             self.add_ants(number, role)
 
@@ -57,9 +65,35 @@ class Environment(Model):
         self.dc = DataCollector(model_reporters=model_reporters)
 
     def get_torus_coordinates(self, x, y):
+        """
+        Gives correct coordinates if the coordinates are out of bounds.
+
+        Args:
+            x: int
+            y: int
+
+        Returns:
+            Tuple of (x, y) that is in ([0, width], [0, height])
+        """
         return x % self.width, y % self.height
 
     def get_torus_neighborhood(self, pos, moore, radius=1, include_center=False):
+        """
+        Faster alternative to the mesa built-in grid.get_neighbourhood().
+
+        Args:
+            pos: tuple of position (int: x, int: y)
+            moore: if True, uses Moore's neighborhood
+                   if False, uses Neumann's neighborhood
+            radius: decides the radius of the neighborhood (default 1)
+            include_center: if True, include the center
+                            if False, do not include the center
+                            (default False)
+
+        Returns:
+            An iterator that gives all coordinates that are connected to pos
+            through the given neighborhood.s
+        """
         x, y = pos
 
         coordinates = set()
@@ -69,6 +103,7 @@ class Environment(Model):
                 if dx == 0 and dy == 0 and not include_center:
                     continue
 
+                # Skip anything outside the manhattan distance for Neumann
                 if not moore and abs(dx) + abs(dy) > radius:
                     continue
 
@@ -84,45 +119,45 @@ class Environment(Model):
 
 
     def get_random_position(self):
-        """docstring for random position."""  # TODO
+        """
+        Gets a random position in the grid, samples from a uniform distribution.
+
+        Returns:
+            Tuple position (int: x, int: y)
+        """
         return (np.random.randint(0, self.width), np.random.randint(0, self.height))
 
     def add_ants(self, N, role):
         """
-        Adds N ants to this colony.
-        :param N: integer value which specifies the nr of ants to add
+        Adds N ants of with role role to this colony.
+
+        Args:
+            N: integer value which specifies the nr of ants to add
+            role: one of {Unassigned, Follower, Leader, Pheromone}
         """
 
         for _ in range(N):
             a = Ant(self.ant_counter, model=self, pos=None, role=role)
-            # print(a.pos)
+
             self.grid.place_agent(a, a.pos)
             self.schedule.add(a)
 
-
             self.ant_counter += 1
-
-        if N == 1:
-            return a
 
     def move_agent(self, ant, pos):
         """
         Move an agent across the map.
-        :param ant: class Ant
-        :param pos: tuple (x, y)
-        """
-        # if self.moore:
-        #     assert np.sum(np.subtract(pos, ant.pos) ** 2) in [1, 2], \
-        #         "the ant can't move from its original position {} to the new position {}, because the distance " \
-        #         "is too large".format(ant.pos, pos)
-        # else:
-        #     assert np.sum(np.subtract(pos, ant.pos) ** 2) == 1, \
-        #         "the ant can't move from its original position {} to the new position {}, because the distance " \
-        #         "is too large, pos_food {}".format(ant.pos, pos, self.food.get_food_pos())
 
+        Args:
+            ant: what agent to move
+            pos: tuple (x, y) to move the agent to
+        """
         self.grid.move_agent(ant, pos)
 
     def get_role_probabilities(self):
+        """
+        Depricated. TODO
+        """
 
         roles_n = [sum([1 if a.role == Unassigned else 0 for a in self.schedule.agents]),
                            0,
@@ -141,6 +176,7 @@ class Environment(Model):
         self.schedule.step()
         self.dc.collect(self)
 
+        # TODO
         if self.grow and self.schedule.steps % 10:
             role_probs = self.get_role_probabilities()
 
@@ -151,24 +187,26 @@ class Environment(Model):
 
     def animate(self, ax):
         """
+        Update the visualization part of the Ants.
 
-        :param ax:
-        :return:
+        Args:
+            ax: axes binding of matplotlib
         """
         self.ax = ax
         self.animate_ants()
 
     def animate_ants(self):
-        """
-        Update the visualization part of the Ants.
-        """
+        """ Ask the ants do update themselfs in the animation. """
         for ant in self.schedule.agents:
             ant.update_vis()
 
     def grid_to_array(self, pos):
         """
         Convert the position/indices on self.grid to imshow array.
-        :param pos: tuple (int: x, int: y)
-        :return: tuple (float: x, float: y)
+
+        Args:
+            pos: tuple (int: x, int: y)
+        Returns:
+            tuple (int: x, int: y)
         """
         return pos[0], self.height - pos[1] - 1
